f61c4f701279920ba5ac2005d14a4afab06d18a5
Luis Guerrero <luis.guerrero@cleteci.com>
2025-10-07 16:55:29 -0400

    chore: add a scrollbar to the homepage

--- lib/ui/homepage/widgets/homepage.dart
+++ lib/ui/homepage/widgets/homepage.dart
@@ -56,113 +56,121 @@ class _HomepageState extends State<Homepage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Divider(height: 100),
-            Column(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //
+              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+              // action in the IDE, or press "p" in the console), to see the
+              // wireframe for each widget.
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const Text('Local Auth Support:'),
-                if (appState.supportState == LocalAuthSupportState.unknown)
-                  const CircularProgressIndicator()
-                else if (appState.supportState ==
-                    LocalAuthSupportState.supported)
-                  const Text('This device is supported')
-                else
-                  const Text('This device is not supported'),
-                const Divider(height: 100),
-                Text('Can check biometrics: ${appState.canCheckBiometrics}\n'),
-                ElevatedButton(
-                  onPressed:
-                      appState.supportState == LocalAuthSupportState.supported
-                      ? appState.checkBiometrics
-                      : null,
-                  child: const Text('Check biometrics'),
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
                 const Divider(height: 100),
-                Text('Available biometrics: ${appState.availableBiometrics}\n'),
-                ElevatedButton(
-                  onPressed:
-                      appState.supportState == LocalAuthSupportState.supported
-                      ? appState.getAvailableBiometrics
-                      : null,
-                  child: const Text('Get available biometrics'),
-                ),
-                const Divider(height: 100),
-                Text('Current State: ${appState.authorized}\n'),
-                if (appState.isAuthenticating)
-                  ElevatedButton(
-                    onPressed: appState.cancelAuthentication,
-                    child: const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text('Cancel Authentication'),
-                        Icon(Icons.cancel),
-                      ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Local Auth Support:'),
+                    if (appState.supportState == LocalAuthSupportState.unknown)
+                      const CircularProgressIndicator()
+                    else if (appState.supportState ==
+                        LocalAuthSupportState.supported)
+                      const Text('This device is supported')
+                    else
+                      const Text('This device is not supported'),
+                    const Divider(height: 100),
+                    Text(
+                      'Can check biometrics: ${appState.canCheckBiometrics}\n',
                     ),
-                  )
-                else
-                  Column(
-                    spacing: 12,
-                    children: <Widget>[
+                    ElevatedButton(
+                      onPressed:
+                          appState.supportState ==
+                              LocalAuthSupportState.supported
+                          ? appState.checkBiometrics
+                          : null,
+                      child: const Text('Check biometrics'),
+                    ),
+                    const Divider(height: 100),
+                    Text(
+                      'Available biometrics: ${appState.availableBiometrics}\n',
+                    ),
+                    ElevatedButton(
+                      onPressed:
+                          appState.supportState ==
+                              LocalAuthSupportState.supported
+                          ? appState.getAvailableBiometrics
+                          : null,
+                      child: const Text('Get available biometrics'),
+                    ),
+                    const Divider(height: 100),
+                    Text('Current State: ${appState.authorized}\n'),
+                    if (appState.isAuthenticating)
                       ElevatedButton(
-                        onPressed:
-                            appState.supportState ==
-                                LocalAuthSupportState.supported
-                            ? appState.authenticate
-                            : null,
+                        onPressed: appState.cancelAuthentication,
                         child: const Row(
                           mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
-                            Text('Authenticate'),
-                            Icon(Icons.perm_device_information),
+                            Text('Cancel Authentication'),
+                            Icon(Icons.cancel),
                           ],
                         ),
-                      ),
-                      ElevatedButton(
-                        onPressed:
-                            appState.supportState ==
-                                LocalAuthSupportState.supported
-                            ? appState.authenticateWithBiometrics
-                            : null,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Text(
-                              appState.isAuthenticating
-                                  ? 'Cancel'
-                                  : 'Authenticate: biometrics only',
+                      )
+                    else
+                      Column(
+                        spacing: 12,
+                        children: <Widget>[
+                          ElevatedButton(
+                            onPressed:
+                                appState.supportState ==
+                                    LocalAuthSupportState.supported
+                                ? appState.authenticate
+                                : null,
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text('Authenticate'),
+                                Icon(Icons.perm_device_information),
+                              ],
                             ),
-                            const Icon(Icons.fingerprint),
-                          ],
-                        ),
+                          ),
+                          ElevatedButton(
+                            onPressed:
+                                appState.supportState ==
+                                    LocalAuthSupportState.supported
+                                ? appState.authenticateWithBiometrics
+                                : null,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  appState.isAuthenticating
+                                      ? 'Cancel'
+                                      : 'Authenticate: biometrics only',
+                                ),
+                                const Icon(Icons.fingerprint),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                  ],
+                ),
               ],
             ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
