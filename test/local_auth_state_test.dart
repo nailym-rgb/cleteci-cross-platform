46d577be6a4523fc67e42408f49432c70d35498c
Jose Alvarado <jose.alvarado@cleteci.com>
2025-10-14 13:13:07 -0400

    unit test and coverage

---
+++ test/local_auth_state_test.dart
@@ -0,0 +1,34 @@
+import 'package:cleteci_cross_platform/ui/auth/view_model/local_auth_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('LocalAuthState', () {
+    late LocalAuthState authState;
+
+    setUp(() {
+      authState = LocalAuthState();
+    });
+
+    test('initial state should be unauthorized', () {
+      expect(authState.authorized, LocalAuthStateValues.unauthorized);
+    });
+
+    test('initial support state should be unknown', () {
+      expect(authState.supportState, LocalAuthSupportState.unknown);
+    });
+
+    test('should have empty available biometrics initially', () {
+      expect(authState.availableBiometrics, isEmpty);
+    });
+
+    test('should not be authenticating initially', () {
+      expect(authState.isAuthenticating, false);
+    });
+
+    test('should not be able to check biometrics initially', () {
+      expect(authState.canCheckBiometrics, false);
+    });
+  });
+}
